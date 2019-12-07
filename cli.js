dbb29b4ff33fc162c17d570ebb44c51e83bc02bc
naturecodevoid <44983869+naturecodevoid@users.noreply.github.com>
2019-12-07 08:37:11 -0800

    cli: Add cli for inserting .eslintrc.js

---
+++ cli.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+require("fs").copyFileSync(
+    require("path").resolve(__dirname, "exports.js"),
+    require("path").resolve(process.cwd(), ".eslintrc.js"),
+);
+console.log("Please run 'npm i -D nature-eslint-config' to finish installing.");
